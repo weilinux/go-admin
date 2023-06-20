f5cdb328ff9977e148eef5b398a56271ec429638
weilin <weilinux2@gmail.com>
2023-06-20 02:06:50 -0800

    为前端处理 返回数据作规范

--- web/middleware/auth.go
+++ web/middleware/auth.go
@@ -18,7 +18,11 @@ func Auth() gin.HandlerFunc {
 		// cookie, err := c.Cookie(UserIDKey)
 		claims, err := validateToken(c.Writer, c.Request)
 		if err != nil {
-			c.Redirect(http.StatusTemporaryRedirect, "/login")
+			data := map[string]interface{}{
+				"Code":    http.StatusUnauthorized,
+				"Message": "未登录啊",
+			}
+			c.JSON(http.StatusOK, data)
 			c.Abort()
 			return
 		}
@@ -33,44 +37,43 @@ func Auth() gin.HandlerFunc {
 // validateToken()参考 一下
 // https://blog.csdn.net/weixin_51507240/article/details/123878839
 
-func validateToken(w http.ResponseWriter, r *http.Request) (claims jwt.MapClaims, err error) {
+func validateToken(_ http.ResponseWriter, r *http.Request) (claims jwt.MapClaims, err error) {
 	if r.Header["Authorization"] == nil {
-		_, _ = fmt.Fprintf(w, "can not find token in header")
-		w.WriteHeader(http.StatusUnauthorized)
+		// _, _ = fmt.Fprintf(w, "can not find token in header")
+		// w.WriteHeader(http.StatusUnauthorized)
 		return nil, errors.New("token error")
 	}
 
 	Token := r.Header["Authorization"][0]
 	if len(Token) <= 7 {
-		w.WriteHeader(http.StatusUnauthorized)
+		// w.WriteHeader(http.StatusUnauthorized)
 		return nil, errors.New("token error")
 	}
 
 	token, err := jwt.Parse(Token[7:], func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			w.WriteHeader(http.StatusUnauthorized)
+			// w.WriteHeader(http.StatusUnauthorized)
 			return nil, fmt.Errorf("there was an error in parsing")
 		}
 		return SampleSecretKey, nil
 	})
 
 	if token == nil {
-		_, _ = fmt.Fprintf(w, "invalid token")
-		w.WriteHeader(http.StatusUnauthorized)
+		// _, _ = fmt.Fprintf(w, "invalid token")
+		// w.WriteHeader(http.StatusUnauthorized)
 		return nil, errors.New("token error")
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
-		_, _ = fmt.Fprintf(w, "couldn't parse claims")
-		w.WriteHeader(http.StatusUnauthorized)
+		// _, _ = fmt.Fprintf(w, "couldn't parse claims")
+		// w.WriteHeader(http.StatusUnauthorized)
 		return nil, errors.New("token error")
 	}
 	exp := claims["exp"].(float64)
 	if int64(exp) < time.Now().Local().Unix() {
-		_, _ = fmt.Fprintf(w, "token expired")
-		w.WriteHeader(http.StatusUnauthorized)
-
+		// _, _ = fmt.Fprintf(w, "token expired")
+		// w.WriteHeader(http.StatusUnauthorized)
 		return nil, errors.New("token error")
 	}
 
