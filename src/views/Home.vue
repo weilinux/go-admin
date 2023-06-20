86e6d155ef6e69eab9a2d34dd943d3fbd55312fe
weilin <weilinux2@gmail.com>
2023-06-20 02:04:59 -0800

    菜单修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,10 @@
 <!--    <img alt="Vue logo" src="../assets/logo.png" />-->
 <!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
 <!--  </div>-->
-  <el-container>
-    <el-header height="50px" style="padding:0">
-      <div class="header">服务器管理系统</div>
-    </el-header>
+  <el-container id="middle-area">
     <el-container width="200px">
-      <el-aside width="200px" style="align:center">
-        <el-menu
+      <el-aside width="200px" style="alignment:center; height: 100%; padding-top: 0px;">
+        <el-menu id="el-menu"
             mode="vertical"
             class="aside"
             router
@@ -36,31 +33,34 @@
           <el-menu-item index="/log">运维工具</el-menu-item>
           <el-menu-item index="7">告警</el-menu-item>
           <el-sub-menu index="8">
-            <template #title>用户</template>
+            <template #title>用户管理</template>
             <el-menu-item index="/login">登录</el-menu-item>
-            <el-menu-item index="/logout">退出</el-menu-item>
-            <el-menu-item index="/signup">注册</el-menu-item>
-            <el-menu-item index="/users">管理</el-menu-item>
+            <el-menu-item index="/register">注册</el-menu-item>
+            <el-menu-item index="/users">用户列表</el-menu-item>
           </el-sub-menu>
-          <el-menu-item index="/about">联系我们</el-menu-item>
         </el-menu>
       </el-aside>
+    </el-container>
 
       <el-container>
-        <el-header height="50px" style="padding:0;margin:0">
-<!--          <div id="header">页面主题</div>-->
-        </el-header>
-        <el-main style="padding:0;margin:0">
+<!--        <el-header height="40px" style="padding:0;margin:0">-->
+<!--&lt;!&ndash;          <div id="header">页面主题</div>&ndash;&gt;-->
+<!--        </el-header>-->
+        <el-main id="el-main" style="padding:0; min-height: 450px;">
           <div class="content">
             <router-view>
             </router-view>
           </div>
         </el-main>
-        <el-footer height="50px" class="footer" style="padding:0">
-          <div>@horse设计</div>
-        </el-footer>
       </el-container>
-    </el-container>
+  </el-container>
+
+
+  <el-container>
+    <el-footer height="30px" class="footer" style="padding:0;background-color: #36332D">
+      <div>@horse设计</div>
+    </el-footer>
+
   </el-container>
 </template>
 
@@ -85,9 +85,6 @@ export default {
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
 #nav a {
   font-weight: bold;
@@ -101,14 +98,26 @@ export default {
 
 .header {
   font-size: 30px;
-  line-height: 50px;
+  line-height: 30px;
   background-color: #f1f1f1;
 }
 
 .aside {
-  background-color: wheat;
-  height: 600px;
+  background-color: #001529;
+  height: 100%;
 }
+
+#el-menu {
+  background-color: #001529;
+}
+
+#middle-area {
+  height: 100%;
+  height: -webkit-calc(100% - 80px);
+  height: -moz-calc(100% - 80px);
+  height: calc(100% - 80px);
+}
+
 
 .subHeader {
   background-color:cornflowerblue;
@@ -122,7 +131,6 @@ export default {
 }
 
 .content {
-  height: 410px;
 }
 
 .footer {
@@ -130,5 +138,8 @@ export default {
   color: white;
   font-size: 17px;
   line-height: 30px;
+  margin-left: 200px;
+  border-top-style: solid;
+  border-top-width: 1px;
 }
 </style>
